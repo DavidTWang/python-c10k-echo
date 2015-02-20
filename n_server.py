9dc336ee14a7b4ad8f68430598b75a21f4bc1bd7
Hakus <archeid@gmail.com>
2015-02-20 14:45:30 -0800

    Added logging features

--- n_server.py
+++ n_server.py
@@ -1,51 +1,62 @@
-import select, socket, sys
+import select, socket, sys, logging
 
-HOST = "192.168.0.16"
+HOST = "localhost"
 PORT = 8005
 BUFFER_SIZE = 1024
 CONNECTIONS = 5
+logging.basicConfig(filename='server.log', filemode='w', format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+dataSent = {}
+dataRecv = {}
+
 
 def echo(connection, address, clientID):
-	data = connection.recv(BUFFER_SIZE)
+	global dataRecv
+	global dataSent
+
+	data = connection.recv(BUFFER_SIZE).rstrip('/n')
 	if(data != ""):
-		print "[%s]Received: %s from %s" % (clientID, data, address)
+		dataRecv[address[0]] += len(data)
+		# print "[%s]Received: %s from %s" % (clientID, data, address)
 		connection.sendall(data)
+		dataSent[address[0]] += len(data)
 	# connection.close()
 
-def select_handling(server):
+# def select_handling(server):
 
-	clientID = 0
+# 	clientID = 0
 
-	try:
-		clients = [server]
-		max_concurrent_clients = 0
+# 	try:
+# 		clients = [server]
+# 		max_concurrent_clients = 0
 
-		while(1):
-			max_concurrent_clients = max(max_concurrent_clients, len(clients)) - 1
-			readList, writeList, exceptions = select.select(clients, [], [])
+# 		while(1):
+# 			max_concurrent_clients = max(max_concurrent_clients, len(clients)) - 1
+# 			readList, writeList, exceptions = select.select(clients, [], [])
 
-			# For each socket ready to be read
-			for sockets in readList:
-				# Ready to accept another client
-				if(sockets == server):
-					while(1):
-						try:
-							conn, address = server.accept()
-							conn.setblocking(0)
-							clients.append(conn)
-							clientID += 1
-						except:
-							break
-				# Echo data from a queued client
-				else:
-					# clients.remove(sockets)
-					echo(sockets, sockets.getpeername(), clientID)
-	finally:
-		for sockets in readList:
-			sockets.close()
-		print "\nMax concurrent clients: {}".format(max_concurrent_clients)
+# 			# For each socket ready to be read
+# 			for sockets in readList:
+# 				# Ready to accept another client
+# 				if(sockets == server):
+# 					while(1):
+# 						try:
+# 							conn, address = server.accept()
+# 							conn.setblocking(0)
+# 							clients.append(conn)
+# 							clientID += 1
+# 						except:
+# 							break
+# 				# Echo data from a queued client
+# 				else:
+# 					# clients.remove(sockets)
+# 					echo(sockets, sockets.getpeername(), clientID)
+# 	finally:
+# 		for sockets in readList:
+# 			sockets.close()
+# 		print "\nMax concurrent clients: {}".format(max_concurrent_clients)
 
 def epoll_handling(server, trigger):
+	global dataRecv
+	global dataSent
 	client_socks = {}
 	max_concurrent_clients = 0
 	epoll = select.epoll()
@@ -74,9 +85,12 @@ def epoll_handling(server, trigger):
 						try:
 							conn, address = server.accept()
 							conn.setblocking(0)
+							epoll.register(conn, conn_flags)
+
 							client_id += 1
 							client_socks[conn.fileno()] = [conn, client_id]
-							epoll.register(conn, conn_flags)
+							dataSent[address[0]] = 0
+							dataRecv[address[0]] = 0
 						except: # socket.error
 							break
 				# Handling inputs from clients
@@ -88,9 +102,22 @@ def epoll_handling(server, trigger):
 				elif(flag & select.EPOLLERR or flag & select.EPOLLHUP):
 					epoll.unregister(fd)
 					client_socks[fd][0].close
+	except:
+		print "Error: ", sys.exc_info()[0]
 	finally:
 		epoll.unregister(server)
 		print "\nMax concurrent clients: {}".format(max_concurrent_clients)
+		print "Client list: "
+		logging.info("Client list: ")
+		for clients in dataRecv.keys():
+			print clients
+			logging.info(clients)
+		for ip, r_data in dataRecv.iteritems():
+			print "Received %s bytes from %s" % (r_data, ip)
+			logging.info("Received %s bytes from %s" % (r_data, ip))
+		for ip, s_data in dataSent.iteritems():
+			print "Sent %s bytes to %s" % (s_data, ip)
+			logging.info("Sent %s bytes to %s" % (s_data, ip))
 
 
 def main():
@@ -100,14 +127,16 @@ def main():
 	server.bind((HOST, PORT))
 	server.setblocking(0)
 
-	method = raw_input("Select or epoll?: ").lower()
+	method = raw_input("Level-trigger[0] or Edge-trigger[1]?: ").lower()
 
 	try:
-		server.listen(CONNECTIONS)
-		if(method == "select"):
-			select_handling(server)
-		elif(method == "epoll"):
-			epoll_handling(server)
+		server.listen(socket.SOMAXCONN)
+		if(method.lower() == "level" or method == "0"):
+			print "Starting level-triggered epoll server..."
+			epoll_handling(server, "level")
+		elif(method.lower() == "edge" or method == "1"):
+			print "Starting edge-triggered epoll server..."
+			epoll_handling(server, "edge")
 	except KeyboardInterrupt:
 		pass
 	finally:
